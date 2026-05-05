0b56c46af1c384fb47ad7a01d7b54656a475028b
Rommel Galisanao <rommel.galisanao.22@gmail.com>
2026-05-05 17:10:14 +0800

    fix: remove manual Log call to prevent permission-related 500 errors

--- backend/app/Http/Controllers/Api/SuperAdminController.php
+++ backend/app/Http/Controllers/Api/SuperAdminController.php
@@ -20,7 +20,6 @@ class SuperAdminController extends Controller
      */
     public function listTenants()
     {
-        \Log::info("Global Admin requested all tenants list.");
         return Tenant::withCount('branches')->get();
     }
 
